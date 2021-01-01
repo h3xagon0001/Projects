f371ecb55b820be06c3bd49c98fc18ff62dbd0e6
oxfordking <62241425+oxfordking@users.noreply.github.com>
2021-01-01 17:29:08 +0800

    Add files via upload

---
+++ Python/Mad Libs.py
@@ -0,0 +1,36 @@
+# varibles
+singularNoun1 = ""
+pluralNoun1 = ""
+adjective1 = ""
+adjective2 = ""
+adjective3 = ""
+noun1 = ""
+verb1 = ""
+verb2 = ""
+verb3 = ""
+verb4 = ""
+verb5 = ""
+verb6 = ""
+verb7 = ""
+
+# input
+print("Let's play a game.")
+singularNoun1 = input("Give me a singular noun.\n")
+verb1 = input("Give me a verb.\n")
+verb2 = input("Give me a another verb.\n")
+verb3 = input("Give me a verb.\n")
+verb4 = input("Give me a another verb.\n")
+adjective1 = input("Give me a adjective.\n")
+verb5 = input("Give me a verb.\n")
+adjective2 = input("Give me another adjective.\n")
+verb6 = input("Give me a verb.\n")
+verb7 = input("Give me another verb.\n")
+pluralNoun1 = input("Give me a plural noun.\n")
+noun1 = input("Give me a noun.\n")
+adjective3 = input("Give me a adjective.\n")
+
+# story
+madlibsStory = (f"The {singularNoun1} wouldn't {verb1}, it would never {verb2} him. It would never {verb3} him or {verb4} at him or get {adjective1} and {verb5} him or say it was too {adjective2} to spend time with him. And it would {verb6} to {verb7} him. Of all the would-be {pluralNoun1} that came over the years, this thing, this {noun1}, was the only thing that {adjective3} up.")
+
+# print story
+print(madlibsStory)
