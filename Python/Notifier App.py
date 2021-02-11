a8dbb64a41cffd41cf87cb2877f3f4f25ebeef73
h3xex <62241425+oxfordking@users.noreply.github.com>
2021-02-11 21:38:52 +0800

    Update Notifier App.py

--- Python/Notifier App.py
+++ Python/Notifier App.py
@@ -2,8 +2,9 @@ import time
 from plyer import notification
 
 def notifyFunction():
+    halfs = 1
     while(True):
-        halfs = 1
+        time.sleep(0.5*60*60
         notification.notify(
             title="Take a break",
             message=f"You should take a break you have been using for {halfs * 30} minutes.",
@@ -12,7 +13,5 @@ def notifyFunction():
             app_icon="F:\Programs\Python\Media\Paomedia-Small-N-Flat-Bell.ico",
             timeout="30"
         )
-        time.sleep(0.5*60*60)
         halfs = halfs + 1
-
 notifyFunction()
