0e578d483fd79277417c79222eccfa0fd887210b
h3xex <62241425+h3xex@users.noreply.github.com>
2021-05-04 07:04:24 +0800

    Add files via upload

--- Python/Text-based Adventure Game.py
+++ Python/Text-based Adventure Game.py
@@ -15,9 +15,6 @@ class Player:
         self.gold = gold
         self.potions = potions
 
-# function when killed by enemy
-def playerDead():
-    pass
 # random enemy function
 def randomEnemy():
     x = random.randint(1, 3)
@@ -38,17 +35,49 @@ def playGame():
     if choice == 1:
         fightEnemy()
 
-    #elif choice == 2:
+    elif choice == 2:
+        town()
 
     elif choice == 3:
         print(f"Health: {player.health}\nDamage: {player.damage}\nGold: {player.gold}\nPotions: {player.potions}")
         playGame()
 
-    #elif choice == 4:
+    elif choice == 4:
+        print("Thanks for playing!")
+        input()
 
-    #else:
-    #    print("Invalid choice")
-    #    playGame()
+    else:
+        print("Invalid choice")
+        playGame()
+
+# function for town
+def town():
+    print("You have found a town in the jungle.")
+    while True:
+        choice = int(input("[1] Inn\n[2] Market\n[3] Leave town\n"))
+        if choice == 1:
+            innChoice = input(f"Do you want to stay in a Inn for 5 gold?(y/n)\nYou have {player.gold} gold.\n")
+            if innChoice == "y" and player.gold >= 5:
+                player.health = 50
+                print("Your wounds have healed.")
+
+        elif choice == 2:
+            marketChoice = int(input(f"What would you like to buy?\nYou have {player.gold} gold.\n[1] +1 Damage (8 gold)\n[2] Health Potion (10 gold)\n[3] Leave market\n"))
+            if marketChoice == 1 and player.gold >= 8:
+                player.gold -= 8
+                player.damage += 1
+                print("Your damage has increased by 1.")
+        
+            elif marketChoice == 2 and player.gold >= 10:
+                player.gold -= 10
+                player.potions += 1
+                print("You have bought a health potion.")
+            
+        elif choice == 3:
+            playGame()
+
+        else:
+            print("Invalid choice")
 
 # function for combat
 def fightEnemy():
@@ -59,19 +88,19 @@ def fightEnemy():
         choice = int(input("[1] Attack\n[2] Items\n[3] Run\n"))
         if choice == 1:
             # deal damage to enemy
-            enemy.health = enemy.health - player.damage
+            enemy.health -= player.damage
             print(f"You attack the {enemy.name} for {player.damage} damage.")
 
             # check if enemy is dead
             if enemy.health <= 0:
                 gold = random.randint(5, 10)
-                player.gold = player.gold + gold
+                player.gold += gold
                 print(f"{enemy.name} has been defeated.")
                 print(f"You have earned {gold} gold.")
                 playGame()
             
             # deal damage to player
-            player.health = player.health - enemy.damage
+            player.health -= enemy.damage
             print(f"The {enemy.name} attacks you for {enemy.damage} damage.")
 
             # check if player is dead
@@ -89,7 +118,7 @@ def fightEnemy():
             print(f"You have {player.potions} potions.")
             x = input("Use a potion?(y/n)\n")
             if x == "y" and player.potions >= 1:
-                player.potions = player.potions - 1
+                player.potions -= 1
                 player.health = 50
 
         elif choice == 3:
@@ -103,4 +132,4 @@ print("")
 print("You wake up in a jungle. You look around with no memory of how you ended up there.")
 playGame()
 
-# thx pZouLL#0001 and JellyFishy#7462
+# thx @pZouLL#0001 and @JellyFishy#7462
