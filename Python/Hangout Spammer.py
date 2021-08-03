2b779fd67888d8426dd18ad079a48be7ee70fcbe
h3xex <62241425+h3xex@users.noreply.github.com>
2021-08-04 07:45:49 +0800

    Add files via upload

---
+++ Python/Hangout Spammer.py
@@ -0,0 +1,16 @@
+from time import time
+import pyautogui
+import time
+
+time.sleep(5)
+while True:
+    pyautogui.write("/corgis")
+    pyautogui.press("enter")
+    pyautogui.write("/ponystream")
+    pyautogui.press("enter")
+    pyautogui.write("/ponies")
+    pyautogui.press("enter")
+    pyautogui.write("/shydino")
+    pyautogui.press("enter")
+    pyautogui.write("/pitchforks")
+    pyautogui.press("enter")
