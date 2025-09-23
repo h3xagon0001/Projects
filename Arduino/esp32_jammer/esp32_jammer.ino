b248c2face7bc72118cc3b12e2616f24495ef06c
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-09-23 10:26:25 +0800

    Add files via upload

--- Arduino/esp32_jammer/esp32_jammer.ino
+++ Arduino/esp32_jammer/esp32_jammer.ino
@@ -19,9 +19,6 @@ SPIClass *hspi_pointer = nullptr;
 RF24 leftRadio(LEFT_CE, LEFT_CSN);
 RF24 rightRadio(RIGHT_CE, RIGHT_CSN);
 
-// dummy address
-const byte address[6] = "00001";
-
 void setup() {
   // put your setup code here, to run once:
 
@@ -35,19 +32,43 @@ void setup() {
   Serial.begin(115200);
 
   if (!leftRadio.begin(vspi_pointer)) {
-    Serial.println("Radio left hardware not responding!");
+    Serial.println("Left radio is not responding.");
     while (1) {} // Hold program in infinite loop
   }
 
   if (!rightRadio.begin(hspi_pointer)) {
-    Serial.println("Radio right hardware not responding!");
+    Serial.println("Right radio is not responding.");
     while (1) {} // Hold program in infinite loop
   }
 
-  Serial.println("WORKS");
+  Serial.println("Both radios initialised successfully.");
+
+  // radio setup
+  leftRadio.stopListening();
+  rightRadio.stopListening();
+  leftRadio.setAutoAck(false);
+  rightRadio.setAutoAck(false);
+  leftRadio.setRetries(0, 0);
+  rightRadio.setRetries(0, 0);
+  leftRadio.setPALevel(RF24_PA_MAX, false);
+  rightRadio.setPALevel(RF24_PA_MAX, false);
+  leftRadio.setDataRate(RF24_2MBPS);
+  rightRadio.setDataRate(RF24_2MBPS);
+  leftRadio.printPrettyDetails();
+  rightRadio.printPrettyDetails();
+  
+  leftRadio.startConstCarrier(RF24_PA_MAX, 0);
+  rightRadio.startConstCarrier(RF24_PA_MAX, 0);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  leftRadio.setChannel(random(126));
+  rightRadio.setChannel(random(126));
 }
+
+
+
+
+
+
