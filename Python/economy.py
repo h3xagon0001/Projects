523fc2e6d70a3c38593b5a1bd1cc9ca5f4cec3a6
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-11-01 21:08:50 +0800

    Update economy.py

--- Python/economy.py
+++ Python/economy.py
@@ -1,8 +1,22 @@
 import random
 
-class Person:
-    def __init__(self, age: int):
-        self.age = age
+class Individual:
+    def __init__(self,
+            age: int = 0,
+            wallet: int = 0,
+            savings: int = 0,
+            income: int = 0,
+            needs: int = 0,
+            wants: int = 0
+    ):
+        self.age        = age
+        self.wallet     = wallet
+        self.savings    = savings
+        self.income     = income
+        self.needs      = needs
+        self.wants      = wants
+        
+
 
     def isDead(self) -> bool:
         # when using this method make sure to roll
@@ -14,5 +28,30 @@ class Person:
             return False
 
 class Household:
-    def __init__(self):
-        pass
+    def __init__(self, members: list[Individual], savings: int=0):
+        self.members = members
+        self.savings = savings
+        for member in members:
+            self.savings += member.savings
+            member.savings = 0
+
+# maybe name, fixed/average/variable cost, revenue, profit, etc.
+class Firm:
+    pass
+
+class Government:
+    def __init__(self, budget: int, taxRate: float, interestRate: float):
+        self.budget         = budget
+        self.taxRate        = taxRate
+        self.interestRate   = interestRate
+        
+
+a = Individual(savings=100)
+b = Individual(savings=150)
+
+print(a.savings, b.savings)
+c = Household(members=[a, b])
+print(c.members)
+print(c.savings)
+
+print(a.savings, b.savings)
