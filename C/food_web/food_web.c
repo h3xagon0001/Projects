d485ef0e18aefb3d74cbfd79dacfde06660dbfc6
Ji Kin <oxfordking2008@gmail.com>
2026-01-05 21:48:20 +0800

    added consuming logic and simple growth

--- C/food_web/food_web.c
+++ C/food_web/food_web.c
@@ -4,7 +4,7 @@ int main() {
 
 	struct organism {
 		const char* name;
-		const int population;
+		unsigned int population;
 		const int consumptionPerIndividual;
 		const char* consumes[2];
 	};
@@ -12,10 +12,10 @@ int main() {
 	#define ORGANISM_COUNT 5
 
 	struct organism ecosystem[ORGANISM_COUNT] = {
-		{"Algae", 100000, 0, {"", ""}},
-		{"Crab", 30000, 10, {"Algae", ""}},
-		{"Shrimp", 25000, 8, {"Algae", ""}},
-		{"Snapper", 4000, 5, {"Shrimp", "Crab"}},
+		{"Algae", 500000, 0, {"", ""}},
+		{"Crab", 30000, 1, {"Algae", ""}},
+		{"Shrimp", 25000, 2, {"Algae", ""}},
+		{"Snapper", 4000, 3, {"Shrimp", "Crab"}},
 		{"Heron", 350, 4, {"Snapper", "Crab"}}
 	};
 
@@ -37,7 +37,33 @@ int main() {
 		}
 	}
 
+	int step(struct organism* ecosystem) {
+		// for each organism
+		for (int i = 0; i < ORGANISM_COUNT; i++) {
+			// for each organism it consumes
+			for (int j = 0; j < 2; j++) {
+				// search thru each organism
+				for (int k = 0; k < ORGANISM_COUNT; k++) {
+					if (ecosystem[i].consumes[j] == ecosystem[k].name) {
+						ecosystem[k].population -= ecosystem[i].population * ecosystem[i].consumptionPerIndividual;
+					}
+				}
+			}
+		
+			ecosystem[i].population *= 2;
+
+		}
+	}
+
 	printEcosystem(ecosystem);
+
+	while (1) {
+		printf("Press ENTER to continue.");
+		getchar();
+
+		step(ecosystem);
+		printEcosystem(ecosystem);
+	}
 
 	return 0;
 
